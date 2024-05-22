9daf79539d85e680a0343bbfbebf89f49e972caa
stui_.dbin._ <l01039428515@gmail.com>
2024-05-22 18:26:09 +0900

    4

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import './Movie.css';
 import { Link, useNavigate } from 'react-router-dom';
