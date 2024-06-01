9db278d697e27bd14f757bf2f1f4d36ffe18eaf9
stui_.dbin._ <l01039428515@gmail.com>
2024-06-01 09:55:54 +0900

    24

--- src/Page/pages/MovieDetailPage.jsx
+++ src/Page/pages/MovieDetailPage.jsx
@@ -84,7 +84,7 @@ const MovieDetailPage = () => {
   };
 
   const getYouTubeId = (url) => {
-    const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+    const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
     const match = url.match(regExp);
     return match && match[2].length === 11 ? match[2] : null;
   };
