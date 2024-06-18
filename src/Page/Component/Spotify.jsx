5f9aa525d71ea022101e41a4f8d1b48838498031
stui_.dbin._ <l01039428515@gmail.com>
2024-06-18 14:19:02 +0900

    스포티파이 열기

--- src/Page/Component/Spotify.jsx
+++ src/Page/Component/Spotify.jsx
@@ -1,51 +1,10 @@
-// import React from 'react';
-// import PropTypes from 'prop-types';
-// import '../CSS/Main.css'; // CSS 파일 경로가 올바른지 확인하세요
-
-// const Spotify = ({ vods = [], handlePosterClick }) => {
-//   const displayVods = (vods) => {
-//     console.log(vods)
-//     return (
-//       <div className="vod-slider">
-//         {vods.map((vod, index) => (
-//           <div key={index} className="vod-item" onClick={() => handlePosterClick(vod.VOD_ID)}>
-//             <img src={vod.POSTER || 'default-poster.jpg'} alt={vod.TITLE} />
-//             <p>{vod.TITLE}</p>
-//           </div>
-//         ))}
-//       </div>
-//     );
-//   };
-
-//   return (
-//     <div className="spotify-container">
-//       <h2>Spotify 감정 추천</h2>
-//       <div className="vod-container">
-//         {vods.length > 0 ? displayVods(vods) : <p>Loading...</p>}
-//       </div>
-//     </div>
-//   );
-// };
-
-// // Spotify.propTypes = {
-// //   vods: PropTypes.arrayOf(
-// //     PropTypes.shape({
-// //       VOD_ID: PropTypes.number.isRequired,
-// //       POSTER: PropTypes.string,
-// //       TITLE: PropTypes.string.isRequired,
-// //     })
-// //   ),
-// //   handlePosterClick: PropTypes.func.isRequired,
-// //};
-
-// export default Spotify;
-
 import React from 'react';
-import '../CSS/Main.css';
+import PropTypes from 'prop-types';
+import '../CSS/Main.css'; // CSS 파일 경로가 올바른지 확인하세요
 
-const Spotify = ({ vods, handlePosterClick }) => {
+const Spotify = ({ vods = [], handlePosterClick }) => {
   const displayVods = (vods) => {
-    console.log("Displaying VODs:", vods); // 데이터를 콘솔에 출력하여 확인
+    console.log(vods)
     return (
       <div className="vod-slider">
         {vods.map((vod, index) => (
@@ -62,10 +21,22 @@ const Spotify = ({ vods, handlePosterClick }) => {
     <div className="spotify-container">
       <h2>Spotify 감정 추천</h2>
       <div className="vod-container">
-        {vods && vods.length > 0 ? displayVods(vods) : <p>Loading...</p>}
+        {vods.length > 0 ? displayVods(vods) : <p>Loading...</p>}
       </div>
     </div>
   );
 };
 
+Spotify.propTypes = {
+  vods: PropTypes.arrayOf(
+    PropTypes.shape({
+      VOD_ID: PropTypes.number.isRequired,
+      POSTER: PropTypes.string,
+      TITLE: PropTypes.string.isRequired,
+    })
+  ),
+  handlePosterClick: PropTypes.func.isRequired,
+};
+
 export default Spotify;
+
